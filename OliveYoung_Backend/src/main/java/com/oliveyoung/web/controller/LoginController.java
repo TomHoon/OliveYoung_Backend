62fe2e369d058022c1fd6e054572690ce8f62bdf
tomhoon <gnsdl9108@naver.com>
2023-09-03 22:44:49 +0900

    add about member login

--- OliveYoung_Backend/src/main/java/com/oliveyoung/web/controller/LoginController.java
+++ OliveYoung_Backend/src/main/java/com/oliveyoung/web/controller/LoginController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oliveyoung.web.dao.MemberDao;
@@ -18,7 +20,42 @@ public class LoginController {
 	@GetMapping("/getMemberAll")
 	public List<MemberEntity> getMemberAll() {
 		List<MemberEntity> list = mDao.getMemberAll();
-		System.out.println("result >>>> " + list);
 		return list;
+	}
+	
+	@PostMapping("/getMemberBySeq")
+	public MemberEntity getMemberBySeq(@RequestBody MemberEntity mEnt) {
+		MemberEntity result = mDao.getMemberBySeq(mEnt);
+		return result;
+	}
+	
+	@PostMapping("/joinMember")
+	public int joinMember(@RequestBody MemberEntity mEnt) {
+		int result = mDao.joinMember(mEnt);
+		return result;
+	}
+	
+	@PostMapping("/checkExist")
+	public int checkExist(@RequestBody MemberEntity mEnt) {
+		int result = mDao.checkExist(mEnt);
+		return result;
+	}
+	
+	@PostMapping("/checkExistNick")
+	public int checkExistNick(@RequestBody MemberEntity mEnt) {
+		int result = mDao.checkExistNick(mEnt);
+		return result;
+	}
+	
+	@PostMapping("/login")
+	public MemberEntity login(@RequestBody MemberEntity mEnt) {
+		MemberEntity result = new MemberEntity(); 
+		result = mDao.login(mEnt);
+		if (result == null){
+			MemberEntity exception = new MemberEntity();
+			exception.setMseq(-1);
+			return exception;
+		}
+		return result;
 	}
 }
--- OliveYoung_Backend/src/main/java/com/oliveyoung/web/dao/MemberDao.java
+++ OliveYoung_Backend/src/main/java/com/oliveyoung/web/dao/MemberDao.java
@@ -8,7 +8,11 @@ import com.oliveyoung.web.entity.MemberEntity;
 
 @Mapper
 public interface MemberDao {
-	
 	public List<MemberEntity> getMemberAll();
+	public MemberEntity getMemberBySeq(MemberEntity mEnt);
+	public int joinMember(MemberEntity mEnt);
+	public int checkExist(MemberEntity mEnt);
+	public int checkExistNick(MemberEntity mEnt);
+	public MemberEntity login(MemberEntity mEnt);
 }
 
