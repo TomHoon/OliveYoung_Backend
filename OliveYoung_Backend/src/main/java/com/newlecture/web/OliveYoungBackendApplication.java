ee5df372aeb58607f78c7c99df4acd125c484ea1
kyk <kykim6014@gmail.com>
2023-09-03 15:24:29 +0900

    first commit

---
+++ OliveYoung_Backend/src/main/java/com/newlecture/web/OliveYoungBackendApplication.java
@@ -0,0 +1,13 @@
+package com.newlecture.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveYoungBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveYoungBackendApplication.class, args);
+	}
+
+}
