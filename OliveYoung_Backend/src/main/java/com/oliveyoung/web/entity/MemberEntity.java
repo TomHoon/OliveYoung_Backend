27c3d7bb0e8b07f93cb4fd947ae1b7965edd7704
git l10000@l10000s.cafe24app.com:l10000_l10000s <kmi5063@naver.com>
2023-09-12 23:21:32 +0900

    제품 controller,dao,entity,mamper 생성

--- OliveYoung_Backend/src/main/java/com/oliveyoung/web/entity/MemberEntity.java
+++ OliveYoung_Backend/src/main/java/com/oliveyoung/web/entity/MemberEntity.java
@@ -1,11 +1,35 @@
 package com.oliveyoung.web.entity;
 
+import java.util.Date;
+
 public class MemberEntity {
 	public int mseq;
 	public String mid;
 	public String mpw;
+	public String member_name;
 	public String nickname;
 	public String email;
+	public String profile_img;
+	public String profile_content;
+	public Date member_reg_date;
+	public Date member_amnd_date;
+	public String member_tel;
+	public String member_address;
+	public String postcode;
+	public String detailAddress;
+	public String extraAddress;
+	public String member_birth;
+	public String member_gender;
+	public String member_grade;
+	public String member_del_yn;
+	public String skin_type;
+	public String skin_tone;
+	public String skin_trouble;
+	public String interest_category;
+	public String admin_yn;
+	public String member_point;
+	public String product_purchase_money;
+	
 	public int getMseq() {
 		return mseq;
 	}
@@ -36,6 +60,172 @@ public class MemberEntity {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
+
+	public java.lang.String getMember_name() {
+		return member_name;
+	}
+
+	public void setMember_name(java.lang.String member_name) {
+		this.member_name = member_name;
+	}
+
+	public java.lang.String getProfile_img() {
+		return profile_img;
+	}
+
+	public void setProfile_img(java.lang.String profile_img) {
+		this.profile_img = profile_img;
+	}
+
+	public java.lang.String getProfile_content() {
+		return profile_content;
+	}
+
+	public void setProfile_content(java.lang.String profile_content) {
+		this.profile_content = profile_content;
+	}
+
+	public Date getMember_reg_date() {
+		return member_reg_date;
+	}
+
+	public void setMember_reg_date(Date member_reg_date) {
+		this.member_reg_date = member_reg_date;
+	}
+
+	public Date getMember_amnd_date() {
+		return member_amnd_date;
+	}
+
+	public void setMember_amnd_date(Date member_amnd_date) {
+		this.member_amnd_date = member_amnd_date;
+	}
+
+	public java.lang.String getMember_tel() {
+		return member_tel;
+	}
+
+	public void setMember_tel(java.lang.String member_tel) {
+		this.member_tel = member_tel;
+	}
+
+	public java.lang.String getMember_address() {
+		return member_address;
+	}
+
+	public void setMember_address(java.lang.String member_address) {
+		this.member_address = member_address;
+	}
+
+	public java.lang.String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(java.lang.String postcode) {
+		this.postcode = postcode;
+	}
+
+	public java.lang.String getDetailAddress() {
+		return detailAddress;
+	}
+
+	public void setDetailAddress(java.lang.String detailAddress) {
+		this.detailAddress = detailAddress;
+	}
+
+	public java.lang.String getExtraAddress() {
+		return extraAddress;
+	}
+
+	public void setExtraAddress(java.lang.String extraAddress) {
+		this.extraAddress = extraAddress;
+	}
+
+	public java.lang.String getMember_birth() {
+		return member_birth;
+	}
+
+	public void setMember_birth(java.lang.String member_birth) {
+		this.member_birth = member_birth;
+	}
+
+	public java.lang.String getMember_gender() {
+		return member_gender;
+	}
+
+	public void setMember_gender(java.lang.String member_gender) {
+		this.member_gender = member_gender;
+	}
+
+	public java.lang.String getMember_grade() {
+		return member_grade;
+	}
+
+	public void setMember_grade(java.lang.String member_grade) {
+		this.member_grade = member_grade;
+	}
+
+	public java.lang.String getMember_del_yn() {
+		return member_del_yn;
+	}
+
+	public void setMember_del_yn(java.lang.String member_del_yn) {
+		this.member_del_yn = member_del_yn;
+	}
+
+	public java.lang.String getSkin_type() {
+		return skin_type;
+	}
+
+	public void setSkin_type(java.lang.String skin_type) {
+		this.skin_type = skin_type;
+	}
+
+	public java.lang.String getSkin_tone() {
+		return skin_tone;
+	}
+
+	public void setSkin_tone(java.lang.String skin_tone) {
+		this.skin_tone = skin_tone;
+	}
+
+	public java.lang.String getSkin_trouble() {
+		return skin_trouble;
+	}
+
+	public void setSkin_trouble(java.lang.String skin_trouble) {
+		this.skin_trouble = skin_trouble;
+	}
+
+	public java.lang.String getInterest_category() {
+		return interest_category;
+	}
+
+	public void setInterest_category(java.lang.String interest_category) {
+		this.interest_category = interest_category;
+	}
+
+	public java.lang.String getAdmin_yn() {
+		return admin_yn;
+	}
+
+	public void setAdmin_yn(java.lang.String admin_yn) {
+		this.admin_yn = admin_yn;
+	}
+
+	public java.lang.String getMember_point() {
+		return member_point;
+	}
+
+	public void setMember_point(java.lang.String member_point) {
+		this.member_point = member_point;
+	}
+
+	public java.lang.String getProduct_purchase_money() {
+		return product_purchase_money;
+	}
+
+	public void setProduct_purchase_money(java.lang.String product_purchase_money) {
+		this.product_purchase_money = product_purchase_money;
+	}
 }
